d7ade9acd2feb99f76a3756b388ade8a0467fb0c
IWasHereFirst <umpiklumpik@gmail.com>
2023-02-25 18:04:58 +0100

    Usage of: WebApplicationContext.SCOPE_SESSION

--- springboot101/src/main/java/com/daniellesko/springboot101/services/RandomNumberService.java
+++ springboot101/src/main/java/com/daniellesko/springboot101/services/RandomNumberService.java
@@ -8,12 +8,10 @@ import org.springframework.web.context.WebApplicationContext;
 import java.util.Random;
 
 @Service
-// WebApplicationContext.SCOPE_REQUEST znamena, ze ked sa vola IndexController, tak @Autowired na tuto triedu neberie jej instanciu
-// priamo z ApllicationContext, ale z Proxy, ktory spravuje ich instancie. Kazde volanie na IndexController vytvori v proxy
-// novu instanciu pre bean RandomNumberService a injectne ju do IndexControlleru
-// ScopedProxyMode.TARGET_CLASS znamena, ze sa vyuzije CGLib proxy, ktora podporuje spravu tried aj interface
-// ScopedProxyMode.INTERFACES znamena, ze sa vyuzije JDK proxy, ktore podporuje len interface
-@Scope(value = WebApplicationContext.SCOPE_REQUEST, proxyMode = ScopedProxyMode.TARGET_CLASS)
+// WebApplicationContext.SCOPE_SESSION - vytvori serverside session vytvorenim cookie, ktora existuje po dobu 25min.
+// Je mozne session nakonfigurovat v application.properties. Reprezentuje Statefull data na servery (zle pre skalovatelnost aplikacie)
+// Proxy vytvori instanciu pre kazdy session.
+@Scope(value = WebApplicationContext.SCOPE_SESSION, proxyMode = ScopedProxyMode.INTERFACES)
 public class RandomNumberService implements MyService {
     private final int value;
 
